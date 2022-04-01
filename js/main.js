772632c3be583a811347f3014af17963447009aa
Jodory <jodory9524@gmail.com>
2022-04-01 23:45:27 +0900

    로그인창수정, 회원가입창, 메인화면 파일 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const loading = document.querySelector(".loading-container")
 const login = document.querySelector('.login')
-const login__h1 = document.querySelector('.login h1')
+const nonMembers = document.querySelector('.login .none-members')
 
 loading.addEventListener('click', function() {
     loading.style.display = 'none';
