d8807ad3cab25f998a5a37dd6d53756c861476a8
Jodory <jodory9524@gmail.com>
2022-04-02 00:02:56 +0900

    main.js 파일 수정

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const loading = document.querySelector(".loading-container")
 const login = document.querySelector('.login')
 const nonMembers = document.querySelector('.login .none-members')
 
+const btn = document.querySelector('.btn')
+const all = document.querySelector('body')
+
 loading.addEventListener('click', function() {
     loading.style.display = 'none';
 
