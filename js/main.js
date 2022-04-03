636a4d6220d78fa2f58741632b4eb473cc0b0d1b
Jodory <jodory9524@gmail.com>
2022-04-03 23:46:47 +0900

    모든 화면에 네비게이션 추가 + 메뉴바에 스톱워치 클릭시 index.html으로 이동

--- js/main.js
+++ js/main.js
@@ -1,90 +1,91 @@
-let timerId;
-let time = 0;
-const stopwatch = document.getElementById("stopwatch");
-let hour, min, sec;
+// let timerId;
+// let time = 0;
+// const stopwatch = document.getElementById("stopwatch");
+// let hour, min, sec;
 
 
-const start = document.querySelector('#start')
-const pause = document.querySelector('#pause')
-const reset = document.querySelector('#reset')
-const main = document.querySelector(".main-start")
-const stopwatchstart = document.querySelector(".stopwatch__menu");
+// const start = document.querySelector('#start')
+// const pause = document.querySelector('#pause')
+// const reset = document.querySelector('#reset')
 
-function printTime() {
-    time++;
-    stopwatch.innerText = getTimeFormatString();
-    // console.log(getTimeFormatString())
-    if (getTimeFormatString() === "00:00:03")
-        alert('Error!')
-}
+// const stopwatchstart = document.querySelector(".stopwatch__menu");
 
-//시계 시작 - 재귀호출로 반복실행
-function startClock() {
-    printTime();
+// function printTime() {
+//     time++;
+//     stopwatch.innerText = getTimeFormatString();
+//     // console.log(getTimeFormatString())
+//     if (getTimeFormatString() === "00:00:03")
+//         alert('Error!')
+// }
 
-    timerId = setTimeout(startClock, 1000);
-}
+// //시계 시작 - 재귀호출로 반복실행
+// function startClock() {
+//     printTime();
 
-//시계 중지
-function pauseClock() {
-    if (timerId != null) {
-        clearTimeout(timerId);
-    }
+//     timerId = setTimeout(startClock, 1000);
+// }
 
-    // let record = "<h2>" + getTimeFormatString() + "</h2>"
-    // document.querySelector(".record").innerHTML = record
+// //시계 중지
+// function pauseClock() {
+//     if (timerId != null) {
+//         clearTimeout(timerId);
+//     }
 
-}
+//     // let record = "<h2>" + getTimeFormatString() + "</h2>"
+//     // document.querySelector(".record").innerHTML = record
 
-// 시계 초기화
-function resetClock() {
-    pauseClock();
-    stopwatch.innerText = "00:00:00";
-    time = 0;
-}
+// }
 
-// 시간(int)을 시, 분, 초 문자열로 변환
-function getTimeFormatString() {
-    hour = parseInt(String(time / (60 * 60)));
-    min = parseInt(String((time - (hour * 60 * 60)) / 60));
-    sec = time % 60;
+// // 시계 초기화
+// function resetClock() {
+//     pauseClock();
+//     stopwatch.innerText = "00:00:00";
+//     time = 0;
+// }
 
-    return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
-}
+// // 시간(int)을 시, 분, 초 문자열로 변환
+// function getTimeFormatString() {
+//     hour = parseInt(String(time / (60 * 60)));
+//     min = parseInt(String((time - (hour * 60 * 60)) / 60));
+//     sec = time % 60;
 
-
-
-start.addEventListener('click', function() {
-    pause.style.display = "contents"
-    reset.style.display = "contents"
-    start.style.display = "none"
-    startClock()
-})
-pause.addEventListener('click', function() {
-    pause.style.display = "none"
-    reset.style.display = "contents"
-    start.style.display = "contents"
-    pauseClock()
-})
-reset.addEventListener('click', function() {
-    main.style.display = 'block'
-
-    stopwatchstart.style.display = 'none'
-    resetClock()
-})
+//     return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
+// }
 
 
-main.addEventListener('click', function() {
-    main.style.display = 'none'
-    stopwatchstart.style.display = 'contents'
-    pause.style.display = "contents"
-    reset.style.display = "contents"
-    start.style.display = "none"
-    startClock()
-})
+
+// start.addEventListener('click', function() {
+//     pause.style.display = "contents"
+//     reset.style.display = "contents"
+//     start.style.display = "none"
+//     startClock()
+// })
+// pause.addEventListener('click', function() {
+//     pause.style.display = "none"
+//     reset.style.display = "contents"
+//     start.style.display = "contents"
+//     pauseClock()
+// })
+// reset.addEventListener('click', function() {
+//     main.style.display = 'block'
+
+//     stopwatchstart.style.display = 'none'
+//     resetClock()
+// })
+
+
+// main.addEventListener('click', function() {
+//     main.style.display = 'none'
+//     stopwatchstart.style.display = 'contents'
+//     pause.style.display = "contents"
+//     reset.style.display = "contents"
+//     start.style.display = "none"
+//     startClock()
+// })
 
 
 // 로딩 창 
+const main = document.querySelector(".main-start")
 const loading = document.querySelector(".loading-container")
 
 const all = document.querySelector('body')
