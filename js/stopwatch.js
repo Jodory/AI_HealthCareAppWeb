573d678df75b698a42dd3e6e7a866c4d94599d54
Jodory <jodory9524@gmail.com>
2022-04-04 01:44:25 +0900

    스톱워치 알림시간 설정

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -5,19 +5,37 @@ let hour, min, sec;
 
 const main_start = document.querySelector('#main-text')
 const main = document.querySelector('.main-start')
-
 const watch = document.querySelector('#watch')
-
 const start = document.querySelector('#start')
 const pause = document.querySelector('#pause')
 const reset = document.querySelector('#reset')
+
+let selected_value;
+let num;
+// const timeset = document.querySelector('.timeset')
+// console.log(timeset)
+var alert_select_value = function(select_obj) {
+    // 우선 selectbox에서 선택된 index를 찾고 
+    var selected_index = select_obj.selectedIndex;
+    // 선택된 index의 value를 찾고 
+    selected_value = select_obj.options[selected_index].value;
+    // 원하는 동작을 수행한다. 여기서는 그냥 alert해주는 식으로만 처리함. 
+    num = "00:00:" + selected_value;
+    console.log(selected_value);
+
+};
+
 
 function printTime() {
     time++;
     stopwatch.innerText = getTimeFormatString();
     // console.log(getTimeFormatString())
-    // if (getTimeFormatString() === "00:00:03")
-    //     alert('Error!')
+    if (getTimeFormatString() === num) {
+        setTimeout(function() {
+            alert(selected_value + "초 완료!")
+        }, 500)
+
+    }
 }
 
 //시계 시작 - 재귀호출로 반복실행
@@ -69,13 +87,13 @@ start.addEventListener('click', function() {
     reset.style.display = "contents"
     start.style.display = "none"
     startClock()
-})
+});
 pause.addEventListener('click', function() {
     pause.style.display = "none"
     reset.style.display = "contents"
     start.style.display = "contents"
     pauseClock()
-})
+});
 reset.addEventListener('click', function() {
     pause.style.display = "none"
     reset.style.display = "none"
