b979e98fcf13c6d2c09a1e76766233841dd7d785
Jodory <jodory9524@gmail.com>
2022-05-18 01:03:54 +0900

    4

---
+++ js/squat.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('.main-start')
+const canvas = document.querySelector('#canvas')
+
+main.addEventListener('click', function() {
+    main.style.display = "none"
+    canvas.style.display = ""
+
+})
