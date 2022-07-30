d9800829f3daab33ddf07a57319710f375085f04
Jodory <jodory9524@gmail.com>
2022-07-30 13:14:06 +0900

    로그인 기능 구현

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 // login.js
+
 const loading = document.querySelector(".loading-container")
 const login = document.querySelector('.login')
 const nonMembers = document.querySelector('.login .none-members')
@@ -22,3 +23,24 @@ btn.addEventListener('click', function() {
     loading.style.display = 'contents';
     login.style.display = 'none'
 })
+
+
+document.querySelector(".btn").addEventListener("click", () => {
+    const id = "jodory9524@gmail.com";
+    const password = "1234";
+
+    if (id == document.querySelector("#id").value) {
+        if (password == document.querySelector("#password").value) {
+            alert("환영합니다!");
+            window.location.href = "/";
+            //document.write("환영합니다!");
+
+        } else {
+            alert("비밀번호가 맞지 않습니다.");
+            //document.write("비밀번호가 맞지 않습니다.");
+        }
+    } else {
+        alert("아이디 혹은 비밀번호가 맞지 않습니다.");
+        //document.wrtie("아이디 혹은 비밀번호가 맞지 않습니다.");
+    }
+});
